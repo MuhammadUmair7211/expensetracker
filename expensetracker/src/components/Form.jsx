23f2b18917a26c1da1252cb05872747b854e5388
Muhammad Umair <muhammadumair7211@gmail.com>
2024-12-01 11:50:09 +0500

    added form validation

--- expensetracker/src/components/Form.jsx
+++ expensetracker/src/components/Form.jsx
@@ -1,20 +1,65 @@
-import React from "react";
-
+import React, { useState } from "react";
 const Form = ({ setExpenses }) => {
+  const [expense, setExpense] = useState({
+    title: "",
+    category: "",
+    amount: "",
+  });
   const handleForm = (e) => {
     e.preventDefault();
-    const expense = { ...getFormData(e.target), id: crypto.randomUUID() };
-    setExpenses((prevState) => [...prevState, expense]);
-    e.target.reset();
-  };
-
-  const getFormData = (form) => {
-    const formData = new FormData(form);
-    let data = {};
-    for (const [key, value] of formData.entries()) {
-      data[key] = value;
+    const validateExpense = validate(expense);
+    if (Object.keys(validateExpense).length != "") {
+      return;
     }
-    return data;
+    setExpenses((prevState) => [
+      ...prevState,
+      { ...expense, id: crypto.randomUUID() },
+    ]);
+
+    // setTitle("");
+    // setCategory("");
+    // setAmount("");
+    // const expense = { ...getFormData(e.target), id: crypto.randomUUID() };
+    // setExpenses((prevState) => [...prevState, expense]);
+    // e.target.reset();
+
+    setExpense({
+      title: "",
+      category: "",
+      amount: "",
+    });
+  };
+  const [errors, setErrors] = useState({});
+  const validate = (formData) => {
+    let errorsData = {};
+    if (formData.title === "") {
+      errorsData.title = "*title is required!";
+    }
+    if (formData.category === "") {
+      errorsData.category = "*please select a category!";
+    }
+    if (formData.amount === "") {
+      errorsData.amount = "*amount is required!";
+    }
+    setErrors(errorsData);
+    return errorsData;
+  };
+  // const getFormData = (form) => {
+  //   const formData = new FormData(form);
+  //   let data = {};
+  //   for (const [key, value] of formData.entries()) {
+  //     data[key] = value;
+  //   }
+  //   return data;
+  // };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setExpense((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+    setErrors({});
   };
   return (
     <div>
@@ -24,29 +69,50 @@ const Form = ({ setExpenses }) => {
             <label htmlFor="" className="d-block">
               Title
             </label>
-            <input type="text" className="w-100" name="title" />
+            <input
+              type="text"
+              className="w-100"
+              value={expense.title}
+              onChange={handleChange}
+              name="title"
+            />
+            <p className="text-danger">{errors.title}</p>
           </div>
           <div className="input my-2">
             <label htmlFor="" className="d-block">
               Category
             </label>
-            <select id="category" className="w-100 my-2 py-1" name="category">
+            <select
+              onChange={handleChange}
+              id="category"
+              className="w-100 my-2 py-1"
+              name="category"
+              value={expense.category}
+            >
               <option value="" hidden>
                 Select Category
               </option>
-              <option value="medicine">Medicine</option>
-              <option value="education">Education</option>
-              <option value="business">Business</option>
-              <option value="clothes">Clothes</option>
-              <option value="grocery">Grocery</option>
-              <option value="bills">Bills</option>
+              <option value="Medicine">Medicine</option>
+              <option value="Education">Education</option>
+              <option value="Business">Business</option>
+              <option value="Clothes">Clothes</option>
+              <option value="Grocery">Grocery</option>
+              <option value="Bills">Bills</option>
             </select>
+            <p className="text-danger">{errors.category}</p>
           </div>
           <div className="input my-2">
             <label htmlFor="" className="d-block">
               Amount
             </label>
-            <input type="text" className="w-100" name="amount" />
+            <input
+              type="text"
+              className="w-100"
+              name="amount"
+              value={expense.amount}
+              onChange={handleChange}
+            />
+            <p className="text-danger">{errors.amount}</p>
           </div>
           <button className="btn btn-primary w-100 border-0 py-2 my-3">
             Add
